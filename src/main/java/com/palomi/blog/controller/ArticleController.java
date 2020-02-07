c36186dac20124f78a42982c6d9e082fa2aaa50a
PaloMiklo <pmmiklas@gmail.com>
2020-02-07 21:15:39 +0100

    get articles

--- src/main/java/com/palomi/blog/controller/ArticleController.java
+++ src/main/java/com/palomi/blog/controller/ArticleController.java
@@ -3,6 +3,9 @@ package com.palomi.blog.controller;
 import com.palomi.blog.model.Article;
 import com.palomi.blog.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +18,13 @@ public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    @GetMapping("/articles")
+    public Page<Article> getArticles(Pageable pageable) {
+        return articleRepository.findAll(pageable);
+    }
+
     @PostMapping("/articles")
-    public Article createArticle(@Valid @RequestBody Article article){
+    public Article createArticle(@Valid @RequestBody Article article) {
         return articleRepository.save(article);
     }
 
